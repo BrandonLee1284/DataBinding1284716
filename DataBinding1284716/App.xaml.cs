3aab4f1b4cbd8c290b6c95bc35348d94fad717ef
Brandon Valladares <brandonvalladares567@hotmail.com>
2024-07-22 23:22:38 -0600

    Agregar archivos de proyecto.

---
+++ DataBinding1284716/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DataBinding1284716
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DataBinding1284716/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataBinding1284716
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
